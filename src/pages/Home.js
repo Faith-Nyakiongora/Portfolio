8e2bfad8d5cffc05f0bd1378fbc7dcd459809ea7
+ <+>
2024-02-09 04:29:28 +0300

    change background and home css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,21 @@
 import React from "react";
 import cover_backg from "../Assets/canvabackg.png";
+import background_cover from "../Assets/background.jpg";
 import girl_ill from "../Assets/girlillustration.png";
 import "../styles/Home.css";
 
 function Home() {
   return (
     <div className="Home">
-      <img src={girl_ill} alt="girl illustration" className="girl-image" />
-      {/* <image src={girl_img} alt="girl illustration" /> */}
-      <img src={cover_backg} alt="background " className="canvas-background" />
+      <div className="col-md-5">
+        <img src={girl_ill} alt="girl illustration" className="girl-image" />
+      </div>
+
+      <img
+        src={background_cover}
+        alt="background "
+        className="canvas-background"
+      />
       <div className="about">
         <h2>Hi, My Name is Faith</h2>
         <div className="prompt">
