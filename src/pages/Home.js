769bbf3dbd489beda2375959c348478aebcfa02c
+ <+>
2024-02-18 11:09:54 +0300

    Added about and tech stack sections

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,11 @@ import background_cover from "../Assets/background.jpg";
 import girl_ill from "../Assets/girlillustration.png";
 import "../styles/Home.css";
 import { AiFillGithub } from "react-icons/ai";
-import { FaLinkedinIn } from "react-icons/fa";
+import { FaLinkedinIn, FaReact } from "react-icons/fa";
+import { SiJavascript, SiFlask } from "react-icons/si";
+import { IoLogoPython } from "react-icons/io5";
+import { IoLogoCss3 } from "react-icons/io5";
+import { IoLogoHtml5 } from "react-icons/io";
 
 function Home() {
   return (
@@ -16,6 +20,8 @@ function Home() {
             alt="background "
             className="canvas-background"
           />
+
+          {/* Introduction Section........... */}
           <Row>
             <Col md={7} className="home-header">
               <h1 style={{ paddingBottom: 15 }} className="heading">
@@ -25,25 +31,67 @@ function Home() {
                 </span>
               </h1>
 
-              <h1 className="heading-name">
+              <h1 style={{ paddingBottom: 50 }} className="heading-name">
                 I'M
                 <strong className="main-name"> Faith Nyakiongora</strong>
               </h1>
 
-              {/* <div style={{ padding: 50, textAlign: "left" }}>
-                <Type />
-              </div> */}
+              <h1 style={{ paddingLeft: 45 }} className="title">
+                {" "}
+                A Sofware Developer
+              </h1>
             </Col>
 
-            <Col md={5} style={{ paddingBottom: 20 }}>
+            <Col md={5} style={{ paddingBottom: 20 }} className="girl-image">
               <img
                 src={girl_ill}
                 alt="girl illustration"
-                className="girl-image"
                 style={{ maxHeight: "450px" }}
               />
             </Col>
           </Row>
+
+          {/* About Me Section */}
+          <Row>
+            <Col md={12} className="home-about" style={{ paddingTop: 100 }}>
+              <h1>ABOUT ME</h1>
+              <p>
+                I am a passionate software developer learning to build web
+                applications using modern technologies. I love learning new
+                things and solving complex problems.
+              </p>
+              <p>
+                Outside of coding, I enjoy art, music, travel, adventure and
+                spending time with family and friends.
+              </p>
+            </Col>
+          </Row>
+
+          {/* Stacks Section ...................*/}
+          <Row>
+            <Col md={12} className="home-stacks">
+              <h1>MY STACKS</h1>
+              <p>Here are some of the technologies I work with:</p>
+              <div className="stack-icons">
+                <FaReact className="stack-icon" />
+                <SiJavascript className="stack-icon" />
+                <IoLogoHtml5 className="stack-icon" />
+                <IoLogoCss3 className="stack-icon" />
+                <IoLogoPython className="stack-icon" />
+                <SiFlask className="stack-icon" />
+              </div>
+              <ul className="stack-list">
+                <li>React</li>
+                <li>Javascript</li>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>Python</li>
+                <li>Flask</li>
+              </ul>
+            </Col>
+          </Row>
+
+          {/* Find Me Section.................. */}
           <Row>
             <Col md={12} className="home-about-social">
               <h1>FIND ME ON</h1>
@@ -78,23 +126,6 @@ function Home() {
       </Container>
     </section>
     // <div className="Home">
-    //   <div className="col-md-5">
-    //     <img src={girl_ill} alt="girl illustration" className="girl-image" />
-    //   </div>
-
-    //   <img
-    //     src={background_cover}
-    //     alt="background "
-    //     className="canvas-background"
-    //   />
-    //   <div className="about">
-    //     <h2>Hi, My Name is Faith</h2>
-    //     <div className="prompt">
-    //       <p>I am a Software Developer</p>
-    //     </div>
-    //   </div>
-    //   <div className="skills"></div>
-    // </div>
   );
 }
 
